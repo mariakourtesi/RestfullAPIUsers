efbf63e53dffc5b579f912296c805877311e0049
Maria <mkourtesi@hotmail.com>
2021-06-27 16:08:50 +0100

    fix: fixed the serviceworker console error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceworker";
 
 ReactDOM.render(
     <BrowserRouter>
